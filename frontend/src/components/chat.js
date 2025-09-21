a358f0d99c94d14e87b2d47aef1b36a05199f678
RohitVenkatMutyala <rohithvenkatmutyala@gmail.com>
2025-09-21 16:20:16 +0530

    nars1

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { toast } from 'react-toastify';
 import { useAuth } from '../context/AuthContext';
 import { useParams, useNavigate } from 'react-router-dom';
 import { db } from '../firebaseConfig'; // Import the firestore instance
